bf5eee6f6f410a96f2dc0a21883572f6ae3a3c34
waleedd32 <valid0@hotmail.com>
2023-01-25 18:32:32 +0200

    added some comment to know how to proceed & paragraph (About, Store, Gmail, Images)

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Google Clone</title>
+        <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to Google Clone</h1>
+
+      {/* Header */}
+      <header className="flex   w-full p-5 justify-between text-sm text-gray-700">
+        {/* Left */}
+        <div className="flex space-x-4 items-center">
+          <p>About</p>
+          <p>Store</p>
+        </div>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <p>Gmail</p>
+          <p>Images</p>
+          {/* Icon */}
+
+          {/* Avatar */}
+        </div>
+      </header>
+
+      {/* Body */}
+      {/* Footer */}
     </div>
   );
 }
