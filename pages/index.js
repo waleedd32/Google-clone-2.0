24ca6d83b1dfb86a4bb256b60a0af0851857d0c6
waleedd32 <valid0@hotmail.com>
2023-01-30 23:48:50 +0200

    changed heroicons version & added an input and icons: SearchIcon,MicrophoneIcon ViewGridIcon

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Home() {
   return (
@@ -22,11 +24,37 @@ export default function Home() {
           <p className="link">Gmail</p>
           <p className="link">Images</p>
           {/* Icon */}
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
           <Avatar url="/avatar.jpg" />{" "}
         </div>
       </header>
 
       {/* Body */}
+      <form className="flex flex-col items-center mt-44 flex-grow w-4/5 ">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+        <div
+          className="flex w-full mt-5 hover:shadow-lg 
+        focus-within:shadow-lg max-w-md rounded-full border
+         border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl"
+        >
+          <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+          <input
+            type="text"
+            className="flex-grow focus:outline-none"
+            // value={input}
+            // onChange={(e) => setInput(e.target.value)}
+          />
+          <MicrophoneIcon
+            className="mr-3 h-6 hidden sm:inline-flex
+        text-blue-500 border-l-2 pl-4 border-gray-300 "
+          />
+        </div>
+      </form>
+
       {/* Footer */}
     </div>
   );
