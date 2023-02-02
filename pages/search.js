0c44aed37f85f92a8e52dc6e411739554e95303f
waleedd32 <valid0@hotmail.com>
2023-02-02 19:26:24 +0200

    created search component

---
+++ pages/search.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import React from "react";
+
+function Search() {
+  return (
+    <div>
+      <Head>
+        <title> Jebordoq Search</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <h1>Search page</h1>
+      {/* Search Results */}
+    </div>
+  );
+}
+
+export default Search;
