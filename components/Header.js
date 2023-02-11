d62db423860a99efdf0b393622a9e41196b366cf
waleedd32 <valid0@hotmail.com>
2023-02-11 23:01:39 +0200

    made header sticky

--- components/Header.js
+++ components/Header.js
@@ -19,37 +19,39 @@ function Header() {
   };
 
   return (
-    <div>
-      <Image
-        src="/Jebordoq.png"
-        height={40}
-        width={120}
-        onClick={() => router.push("/")}
-        className="cursor-pointer "
-      />
-      <form
-        className="flex flex-grow px-6 py-3 ml-10 mr-5 border
+    <header className="sticky top-0 bg-white">
+      <div>
+        <Image
+          src="/Jebordoq.png"
+          height={40}
+          width={120}
+          onClick={() => router.push("/")}
+          className="cursor-pointer "
+        />
+        <form
+          className="flex flex-grow px-6 py-3 ml-10 mr-5 border
        border-gray-200 rounded-full shadow-lg max-w-3xl  items-center"
-      >
-        <input
-          ref={searchInputRef}
-          className="flex-grow w-full focus:outline-none"
-          type="text"
-          placeholder="Search"
-        />
-        <XIcon
-          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition 
+        >
+          <input
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+            type="text"
+            placeholder="Search"
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition 
         duration-100 transform hover:scale-125 "
-          onClick={() => (searchInputRef.current.value = "")}
-        />
-        <MicrophoneIcon
-          className="mr-3 h-6 hidden sm:inline-flex
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon
+            className="mr-3 h-6 hidden sm:inline-flex
         text-blue-500 border-l-2 pl-4 border-gray-300 "
-        />
-        <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
-        <button hidden type="submit" onClick={search}></button>
-      </form>
-    </div>
+          />
+          <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+          <button hidden type="submit" onClick={search}></button>
+        </form>
+      </div>
+    </header>
   );
 }
 
