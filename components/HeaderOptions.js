011684edf0a2b4e52d093432bfda4ed083e80d7a
waleedd32 <valid0@hotmail.com>
2023-02-16 23:54:07 +0200

    added some comment and two p tags for Settings and Tools which uses global talwind css

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function HeaderOptions() {
-  return <div>HeaderOptions</div>;
+  return (
+    <div>
+      {/* Left */}
+      {/* Right */}
+
+      <p className="link">Settings</p>
+      <p className="link">Tools</p>
+    </div>
+  );
 }
 
 export default HeaderOptions;
