2c90dfa52098665fa2dfe995fb5e2f43e7cd7c5e
waleedd32 <valid0@hotmail.com>
2023-02-21 16:17:16 +0200

    added styles to HeaderOption such as centering, hover and margin

--- components/HeaderOption.js
+++ components/HeaderOption.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function HeaderOption({ Icon, title, selected }) {
   return (
-    <div className={`flex`}>
+    <div
+      className={`flex items-center space-x-1
+    border-b-4 border-transparent
+     hover:text-blue-500 hover:border-blue-500
+      pb-3 cursor-pointer `}
+    >
       <Icon className="h-4" />
       <p className="hidden sm:inline-flex">{title}</p>
     </div>
