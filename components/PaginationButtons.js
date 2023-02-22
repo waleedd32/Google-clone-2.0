acef7211e5cb844ad62908d2f8beb00d89cc66a0
waleedd32 <valid0@hotmail.com>
2023-02-23 00:44:32 +0200

    added left and right icons which clicked takes to next or previous page

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Link from "next/link";
+
+import { useRouter } from "next/router";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 function PaginationButtons() {
-  return <div>PaginationButtons</div>;
+  const router = useRouter();
+  const startIndex = Number(router.query.start) || 0;
+
+  return (
+    <div>
+      {startIndex >= 10 && (
+        <Link
+          href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
+        >
+          <div>
+            <ChevronLeftIcon />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}`}>
+        <div>
+          <ChevronRightIcon />
+          <p>Next</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default PaginationButtons;
