447a507547fcc8a80483c92a7f0bccf5342a7d46
waleedd32 <valid0@hotmail.com>
2023-02-22 20:25:15 +0200

    created About section where is number of search results and searchTime

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function SearchResults() {
-  return <div>SearchResults</div>;
+function SearchResults({ results }) {
+  return (
+    <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+      <p className="text-gray-600 text-md mb-5 mt-3">
+        About {results.searchInformation?.formattedTotalResults} Results (
+        {results.searchInformation?.formattedSearchTime} seconds){" "}
+      </p>
+    </div>
+  );
 }
 
 export default SearchResults;
